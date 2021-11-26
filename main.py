5f1a5be69b89387acf5eede4ba8284ebf61bf819
crdguez <carlos.rodriguez.jaso@gmail.com>
2021-11-26 16:16:36 +0100

    z

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import pandas as pd
 import numpy as np
 
 st.title('Resultados de natación')
+
+st.header('Última competición')
+
+st.write('**Nadadores** por club y año:')
+
+df=pd.read_csv('https://gitlab.com/crdguez/resultados_natacion/-/raw/main/prueba.csv')
+# st.write(df)
+
+st.write(df.pivot_table(values = 'Nombre', columns='M_F', index=['Club','Anyo_nac'], aggfunc=lambda x: len(x.unique())).unstack().fillna(0).astype(int))
