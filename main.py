96e998724f9b68b8db9cbbf00e818c3f1646d42e
crdguez <carlos.rodriguez.jaso@gmail.com>
2021-11-28 10:58:29 +0100

    z

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ df=pd.read_csv('https://gitlab.com/crdguez/resultados_natacion/-/raw/main/prueba
 # st.write(df)
 
 st.write(df.pivot_table(values = 'Nombre', columns='M_F', index=['Club','Anyo_nac'], aggfunc=lambda x: len(x.unique())).unstack().fillna(0).astype(int))
+
+st.write('Filtro de **Resultados**:')
+
+bu = st.textbox('Buscar:')
+# bu = st.textbox('Buscar:',list(datos_act['Alumno'].unique()))
+
+st.write(df[df.Nombre.str.contains(bu)])
