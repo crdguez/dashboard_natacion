7afca0d27c346aa98b961fd7e2f6587e32a9981a
crdguez <carlos.rodriguez.jaso@gmail.com>
2022-01-08 09:08:26 +0100

    z

--- funciones.py
+++ funciones.py
@@ -50,14 +50,14 @@ def graficas_resumen(df) :
 
     # Distribución de nadadores
     ax3=plt.subplot(gs[1,1])
-    df2=df.pivot_table(values = 'Nombre', columns=['Categoria'], index=['Club'], aggfunc=lambda x: len(x.unique())).fillna(0).astype(int)
-    plt.setp(ax3.xaxis.get_majorticklabels(), rotation=90)
-    bot=np.zeros(len(df2.index)).astype(int)
-    for i, c in enumerate(df2.columns):
-        ax3.bar(df2.index, df2[c], label=c, bottom =bot)
-        bot=bot+df2[c]
-    ax3.legend(loc='best',bbox_to_anchor=(-0.1, 1))
-    ax3.set_title('Distribución del número de nadadores')
+    # df2=df.pivot_table(values = 'Nombre', columns=['Categoria'], index=['Club'], aggfunc=lambda x: len(x.unique())).fillna(0).astype(int)
+    # plt.setp(ax3.xaxis.get_majorticklabels(), rotation=90)
+    # bot=np.zeros(len(df2.index)).astype(int)
+    # for i, c in enumerate(df2.columns):
+    #     ax3.bar(df2.index, df2[c], label=c, bottom =bot)
+    #     bot=bot+df2[c]
+    # ax3.legend(loc='best',bbox_to_anchor=(-0.1, 1))
+    # ax3.set_title('Distribución del número de nadadores')
 
 
     # fg.suptitle('Gráficas resumen', fontsize=16)
